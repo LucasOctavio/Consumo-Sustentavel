0834a6978f415e29dce4e2e667a2ab4f344b2016
juanzink7l <camyloti@gmail.com>
2026-04-29 09:02:07 -0300

    fix

--- src/services/usuario_service.py
+++ src/services/usuario_service.py
@@ -170,6 +170,7 @@ def fun_update(dados, user_id, session):
         # criptografa senha
         dados.user_senha = bcrypt_context.hash(dados.user_senha)
 
+        dados.dict = {k: v for k, v in dados_brutos.items() if v != ""}
     # para cada informacao enviada pelo usuario
     for key, value in dados.dict(exclude_unset=True).items():
         # ele verifica se tem campos vazios nas informacoes passadas
